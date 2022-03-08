24080d135983eb57c7f5f860d2719da46c66dfab
Sam Varghese <sam.varghese@walmart.com>
2022-03-08 11:03:29 -0800

    Updated paths

---
+++ _next/static/06JpRxqE9Gh8c0jzL8AtO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/bc69aac586c645b3.css","static/chunks/pages/index-df3dd936c2f195c5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
